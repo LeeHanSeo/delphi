913fc612c85660015f156061ebdd07ef08b50396
LeeHanSeo <dlgksgh213@gmail.com>
2019-05-22 01:22:54 +0900

    12. DLL

---
+++ 12. DLL/Test_01/uSample.pas
@@ -0,0 +1,41 @@
+unit uSample;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  iDllhandle : Integer;
+  DllFunc : Function(a,b:Integer):Integer;
+begin
+  iDllhandle := LoadLibrary('Test_01.dll');
+  if iDllhandle >= 32 then begin
+    @DllFunc := GetProcAddress(iDllhandle, 'Sum');
+    if @DllFunc <> Nil then
+      ShowMessage(IntToStr(DllFunc(1, 2)));
+      FreeLibrary(iDllhandle);
+  end;
+
+end;
+
+end.
---
+++ 12. DLL/Test_02/Unit1.dfm
@@ -0,0 +1,25 @@
+object Form1: TForm1
+  Left = 279
+  Top = 161
+  Width = 609
+  Height = 490
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Memo1: TMemo
+    Left = 72
+    Top = 112
+    Width = 185
+    Height = 89
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 0
+  end
+end
